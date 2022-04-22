3166e5926d5a681323dfbe3881a8d946b2f58dda
mutotu <muttalip.aydemir@gmail.com>
2022-04-22 20:58:19 +0100

    added auth, counter,header,userprofile

---
+++ src/components/Counter.js
@@ -0,0 +1,37 @@
+import { useSelector, useDispatch } from "react-redux";
+import classes from "./Counter.module.css";
+import { counterActions } from "../store/index";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.showcounter.showcounter);
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(5));
+  };
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <div>
+        <button onClick={incrementHandler}>increment</button>
+        <button onClick={increaseHandler}>increment by 5</button>
+
+        <button onClick={decrementHandler}>decrement</button>
+      </div>
+    </main>
+  );
+};
+
+export default Counter;
